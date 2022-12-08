3f0fd5925e8434223310f1651072e0a5d3862c2b
Nicolas Blaisdell <nblaisdell2@gmail.com>
2022-12-08 12:10:51 -0500

    new error message for getAPIData

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -67,7 +67,7 @@ export async function getAPIData(storedProcName, params, isMutation) {
     })
     .catch((err) => {
       console.log("error in getAPIData");
-      console.log(err.response);
+      // console.log(err.response);
       console.log(err.response.data);
       console.log(err.response.data.errMsg);
     });
